f4b55700a1ae169788986debb85baad8b988ea4e
Stefan_RC <srogers@mrc-lmb.cam.ac.uk>
2025-05-14 12:47:36 +0100

    plotting timeouts from basic log files

---
+++ hex_behav_analysis/basic_analysis/analyse_timeouts.py
@@ -0,0 +1,7 @@
+from hex_behav_analysis.utils.Cohort_folder import Cohort_folder
+
+cohort_directory = "/cephfs2/dwelch/Behaviour/2501_Lynn_EXCIT£/Experiment"
+
+cohort = Cohort_folder(cohort_directory)
+
+print()
