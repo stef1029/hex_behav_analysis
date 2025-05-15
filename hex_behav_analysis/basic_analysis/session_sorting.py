8f2c5ec9658caed279fde5880ad4906da2d3be91
Daniel Welch <dwelch@mrc-lmb.cam.ac.uk>
2025-05-15 19:52:51 +0100

    plotted lynns data

--- hex_behav_analysis/basic_analysis/session_sorting.py
+++ hex_behav_analysis/basic_analysis/session_sorting.py
@@ -87,24 +87,29 @@ def main():
     group_1 = ["wtjp280-4a", "wtjp271-5b", "wtjp271-5c"]
     group_2 = ["wtjp280-4b", "wtjp271-5d", "wtjp280-4f"]
 
-    group_1_baseline_sessions = {"name": "Group 1 Baseline", "dates": ["250127", "250128", "250129", "250130", "250131", "250203", "250205"], "mice": group_1}
-    group_2_baseline_sessions = {"name": "Group 2 Baseline", "dates": ["250127", "250128", "250129", "250130", "250131", "250203", "250205"], "mice": group_2}
+    # group_1_baseline_sessions = {"name": "Group 1 Baseline", "dates": ["250127", "250128", "250129", "250130", "250131", "250203", "250205"], "mice": group_1}
+    # group_2_baseline_sessions = {"name": "Group 2 Baseline", "dates": ["250127", "250128", "250129", "250130", "250131", "250203", "250205"], "mice": group_2}
     group_1_saline = {"name": "Group 1 saline", "dates": ["250206", "250207"], "mice": group_1}
-    group_1_1ng = {"name": "Group 1 1ng", "dates": ["250219", "250220"], "mice": group_1}
+    # group_1_1ng = {"name": "Group 1 1ng", "dates": ["250219", "250220"], "mice": group_1}
     group_1_10ng = {"name": "Group 1 10ng", "dates": ["250208", "250209"], "mice": group_1}
     group_1_100ng = {"name": "Group 1 100ng", "dates": ["250210", "250211"], "mice": group_1}
     group_1_500ng = {"name": "Group 1 500ng", "dates": ["250212", "250213"], "mice": group_1}
     group_1_1000ng = {"name": "Group 1 1000ng", "dates": ["250214", "250215"], "mice": group_1}
     group_2_saline = {"name": "Group 2 saline", "dates": ["250214", "250215"], "mice": group_2}
-    group_2_1ng = {"name": "Group 2 1ng", "dates": ["250219", "250220"], "mice": group_2}
+    # group_2_1ng = {"name": "Group 2 1ng", "dates": ["250219", "250220"], "mice": group_2}
     group_2_10ng = {"name": "Group 2 10ng", "dates": ["250212", "250213"], "mice": group_2}
     group_2_100ng = {"name": "Group 2 100ng", "dates": ["250210", "250211"], "mice": group_2}
     group_2_500ng = {"name": "Group 2 500ng", "dates": ["250208", "250209"], "mice": group_2}
     group_2_1000ng = {"name": "Group 2 1000ng", "dates": ["250206", "250207"], "mice": group_2}
 
-    session_groups = [group_1_baseline_sessions, group_2_baseline_sessions,
-                    group_1_saline, group_1_1ng, group_1_10ng, group_1_100ng, group_1_500ng, group_1_1000ng,
-                    group_2_saline, group_2_1ng, group_2_10ng, group_2_100ng, group_2_500ng, group_2_1000ng]
+    # session_groups = [group_1_baseline_sessions, group_2_baseline_sessions,
+    #                 group_1_saline, group_1_1ng, group_1_10ng, group_1_100ng, group_1_500ng, group_1_1000ng,
+    #                 group_2_saline, group_2_1ng, group_2_10ng, group_2_100ng, group_2_500ng, group_2_1000ng]
+
+    session_groups = [group_1_saline, group_1_10ng, group_1_100ng, group_1_500ng, group_1_1000ng,
+                    group_2_saline, group_2_10ng, group_2_100ng, group_2_500ng, group_2_1000ng]
+    
+    session_groups = {group["name"]: group for group in session_groups}
 
     if print_sessions:
 
@@ -128,9 +133,9 @@ def main():
         colors = [Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.RED, Fore.GREEN, Fore.YELLOW]
 
         for session_group in session_groups:
-            name = session_group["name"]
-            mice = session_group["mice"]
-            dates = session_group["dates"]
+            name = session_groups[session_group]["name"]
+            mice = session_groups[session_group]["mice"]
+            dates = session_groups[session_group]["dates"]
             print(f"\n{Fore.WHITE}{Style.BRIGHT}{name}{Style.RESET_ALL}")
             
             # Create a dictionary to track mouse to color mapping
@@ -157,13 +162,13 @@ def main():
                         # Print with the appropriate color
                         print(f"{color}Session: {session}: {metadata}{Style.RESET_ALL}")
 
-    group_1_baseline_sessions["sessions"] = ["250127_181421_wtjp271-5b", "250128_113115_wtjp271-5b", "250129_191702_wtjp271-5b", "250130_170712_wtjp271-5b", "250131_183201_wtjp271-5b", "250203_175639_wtjp271-5b", "250205_173227_wtjp271-5b",
-                                            "250127_185021_wtjp271-5c", "250128_113116_wtjp271-5c", "250129_191702_wtjp271-5c", "250130_170712_wtjp271-5c", "250131_183201_wtjp271-5c", "250203_175639_wtjp271-5c", "250205_173227_wtjp271-5c",
-                                            "250127_145632_wtjp280-4a", "250128_082732_wtjp280-4a", "250129_164140_wtjp280-4a", "250130_142223_wtjp280-4a", "250131_162811_wtjp280-4a", "250203_160202_wtjp280-4a", "250205_155943_wtjp280-4a"]
+    # group_1_baseline_sessions["sessions_ctrl"] = ["250127_181421_wtjp271-5b", "250128_113115_wtjp271-5b", "250129_191702_wtjp271-5b", "250130_170712_wtjp271-5b", "250131_183201_wtjp271-5b", "250203_175639_wtjp271-5b", "250205_173227_wtjp271-5b",
+    #                                         "250127_185021_wtjp271-5c", "250128_113116_wtjp271-5c", "250129_191702_wtjp271-5c", "250130_170712_wtjp271-5c", "250131_183201_wtjp271-5c", "250203_175639_wtjp271-5c", "250205_173227_wtjp271-5c",
+    #                                         "250127_145632_wtjp280-4a", "250128_082732_wtjp280-4a", "250129_164140_wtjp280-4a", "250130_142223_wtjp280-4a", "250131_162811_wtjp280-4a", "250203_160202_wtjp280-4a", "250205_155943_wtjp280-4a"]
 
-    group_2_baseline_sessions["sessions"] = ["250127_173355_wtjp271-5d", "250128_100504_wtjp271-5d", "250129_175317_wtjp271-5d", "250130_161447_wtjp271-5d", "250131_173050_wtjp271-5d", "250203_165929_wtjp271-5d", "250205_190259_wtjp271-5d",
-                                                "250127_154906_wtjp280-4b", "250128_082732_wtjp280-4b", "250129_164140_wtjp280-4b", "250130_142223_wtjp280-4b", "250131_162810_wtjp280-4b", "250203_160202_wtjp280-4b", "250205_155943_wtjp280-4b",
-                                                "250127_163919_wtjp280-4f", "250128_110948_wtjp280-4f", "250129_184500_wtjp280-4f", "250130_161448_wtjp280-4f", "250131_173050_wtjp280-4f", "250203_165929_wtjp280-4f", "250205_190300_wtjp280-4f"]
+    # group_2_baseline_sessions["sessions_ctrl"] = ["250127_173355_wtjp271-5d", "250128_100504_wtjp271-5d", "250129_175317_wtjp271-5d", "250130_161447_wtjp271-5d", "250131_173050_wtjp271-5d", "250203_165929_wtjp271-5d", "250205_190259_wtjp271-5d",
+    #                                             "250127_154906_wtjp280-4b", "250128_082732_wtjp280-4b", "250129_164140_wtjp280-4b", "250130_142223_wtjp280-4b", "250131_162810_wtjp280-4b", "250203_160202_wtjp280-4b", "250205_155943_wtjp280-4b",
+    #                                             "250127_163919_wtjp280-4f", "250128_110948_wtjp280-4f", "250129_184500_wtjp280-4f", "250130_161448_wtjp280-4f", "250131_173050_wtjp280-4f", "250203_165929_wtjp280-4f", "250205_190300_wtjp280-4f"]
 
 
     group_1_saline["sessions_ctrl"] = ["250206_163413_wtjp271-5b", "250206_165447_wtjp271-5b", "250207_124249_wtjp271-5b", "250207_130006_wtjp271-5b",
@@ -176,26 +181,28 @@ def main():
 
 
     group_1_10ng["sessions_ctrl"] = ["250208_134802_wtjp271-5b", "250209_120731_wtjp271-5b",
-                                        "250208_134803_wtjp271-5c", "250209_120731_wtjp271-5c"]
+                                        "250208_134803_wtjp271-5c", "250209_120731_wtjp271-5c",
+                                        "250208_125823_wtjp280-4a", "250209_112807_wtjp280-4a",]
 
     group_1_10ng["sessions_test"] = ["250208_172218_wtjp271-5b", "250209_152119_wtjp271-5b",
-                                        "250208_172219_wtjp271-5c", "250209_152120_wtjp271-5c"]
+                                        "250208_172219_wtjp271-5c", "250209_152120_wtjp271-5c",
+                                        "250208_153047_wtjp280-4a", "250209_133023_wtjp280-4a"]
 
 
-    group_1_100ng["sessions_ctrl"] = ["250210_151858_wtjp271-5b",
-                                        "250211_152549_wtjp271-5b",
-                                        "250210_151859_wtjp271-5c",
-                                        "250211_152549_wtjp271-5c",
-                                        "250210_131640_wtjp280-4a",
-                                        "250211_133312_wtjp280-4a"]
-
-
-    group_1_100ng["sessions_test"] = ["250210_113704_wtjp271-5b",
+    group_1_100ng["sessions_ctrl"] = ["250210_113704_wtjp271-5b",
                                         "250211_115517_wtjp271-5b",
                                         "250210_113705_wtjp271-5c",
                                         "250211_115518_wtjp271-5c",
                                         "250210_105724_wtjp280-4a",
                                         "250211_110617_wtjp280-4a"]
+
+
+    group_1_100ng["sessions_test"] = ["250210_151858_wtjp271-5b",
+                                        "250211_152549_wtjp271-5b",
+                                        "250210_151859_wtjp271-5c",
+                                        "250211_152549_wtjp271-5c",
+                                        "250210_131640_wtjp280-4a",
+                                        "250211_133312_wtjp280-4a"]
 
 
     group_1_500ng["sessions_ctrl"] = ["250212_112526_wtjp271-5b",
@@ -311,4 +318,13 @@ def main():
                                         "250206_232054_wtjp280-4f",
                                         "250207_182039_wtjp280-4f"]
     
+    print(f"Loaded {len(session_groups)}session groups")
+    print(f"Structure of dicts: dict_keys(['name', 'dates', 'mice', 'sessions_ctrl', 'sessions_test'])")
+    print("Session group names:")
+    for group in session_groups.keys():
+        print(group)
+    
     return session_groups
+
+if __name__ == "__main__":
+    main()
